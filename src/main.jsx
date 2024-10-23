21be234f312305db5d344a96adf9c283ef310406
SteveBrackenridge <138868153+SteveBrackenridge@users.noreply.github.com>
2024-10-23 20:02:50 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}  
+
+      redirectUri={process.env.REACT_APP_AUTH0_REDIRECT_URI}
+    >
+      <App />
+    </Auth0Provider>
+    
+  </StrictMode>,
 )
